50c88ca0d9c17c38ed9211a0fa4214bdbe293f8b
ahmedvarin97 <ahmed.varin.projects@gmail.com>
2025-09-18 23:28:46 +0500

    Movie Detail Page

--- services/api.ts
+++ services/api.ts
@@ -27,10 +27,20 @@ export const fetchMovies = async({query}: {query: string}) => {
 
 export const fetchMovieDetails = async(movieId: string): Promise<MovieDetails> => {
     try {
-        const response = await fetch(`${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`, {
-            method: 'GET',
-            headers: TMDB_CONFIG.HEADERS
-        })
+         const response = await fetch(
+            `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+            {
+                method: "GET",
+                headers: TMDB_CONFIG.HEADERS,
+            }
+        );
+
+        if (!response.ok) {
+        throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.log(error)
         throw(error)
